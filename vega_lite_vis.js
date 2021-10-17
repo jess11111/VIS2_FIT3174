5f1b1e09c15f02aff0d76aede71b258b77056505
jess11111 <50970765+jess11111@users.noreply.github.com>
2021-10-17 20:25:02 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,69 @@
+var vg_1 = "choropleth2.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "barchart1.vega";
+vegaEmbed("#barchart1", vg_2).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "linechart.vega";
+vegaEmbed("#linechart", vg_3).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "linechart_pricepermeter.vega";
+vegaEmbed("#linechart2", vg_4).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "propsymbolmap.vega";
+vegaEmbed("#map2", vg_5).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "dotmap2.vg.json";
+vegaEmbed("#map3", vg_6).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_7 = "bubble.vega";
+vegaEmbed("#bubble", vg_7).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_8 = "dotmap3.vg.json";
+vegaEmbed("#map4", vg_8).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_9 = "stackedbarchart.vega";
+vegaEmbed("#stackedbar", vg_9).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_10 = "stackedbarchart2.vega";
+vegaEmbed("#stackedbar2", vg_10).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_11 = "heatmap2.vega";
+vegaEmbed("#heatmap2", vg_11).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_12 = "bubble2.vega";
+vegaEmbed("#bubble2", vg_12).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_13 = "propsymbolmap3.vega";
+vegaEmbed("#map5", vg_13).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_14 = "stackedbarchart3.vega";
+vegaEmbed("#stackedbar3", vg_14).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
